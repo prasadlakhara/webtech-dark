82f84680f3e75fc4a226055f81416d6d4e96cd64
prasad <prasadlakhara@gmail.com>
2024-12-30 17:44:01 +0530

    Added sliders and updated theme css & added images

---
+++ script/swiper.js
@@ -0,0 +1,15 @@
+
+  var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    effect: 'fade',
+    loop: true,
+    speed: 300,
+    mousewheel: false,
+    pagination: false,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
